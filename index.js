1d7c6e81c7383ece76b8c59aa506f60340e7221d
Tim Kurvers <tim@moonsphere.net>
2015-09-12 15:17:07 +0200

    Disallow dangling commas

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const merge = require('merge');
 const airbnb = require('eslint-config-airbnb/base');
 
 const timkurvers = {
-  rules: {}
+  rules: {
+    // Disallow dangling commas
+    'comma-dangle': [2, 'never']
+  }
 };
 
 module.exports = merge.recursive(airbnb, timkurvers);
