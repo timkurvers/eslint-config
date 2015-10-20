800db36222ff1fd14641fbc9d75c3980c01189f6
Tim Kurvers <tim@moonsphere.net>
2015-10-20 23:48:32 +0200

    Refactor using base rules

---
+++ base.js
@@ -0,0 +1,18 @@
+module.exports = {
+  rules: {
+    // Disallow dangling commas
+    'comma-dangle': [2, 'never'],
+
+    // Allow anonymous functions
+    'func-names': [0],
+
+    // Allow short identifiers
+    'id-length': [0],
+
+    // Enforce curly spacing in object literals and destructuring
+    'object-curly-spacing': [2, 'always'],
+
+    // Allow padded blocks
+    'padded-blocks': [0]
+  }
+};
--- index.js
+++ index.js
@@ -1,19 +1,6 @@
 module.exports = {
-  extends: 'airbnb/base',
-  rules: {
-    // Disallow dangling commas
-    'comma-dangle': [2, 'never'],
-
-    // Allow anonymous functions
-    'func-names': [0],
-
-    // Allow short identifiers
-    'id-length': [0],
-
-    // Enforce curly spacing in object literals and destructuring
-    'object-curly-spacing': [2, 'always'],
-
-    // Allow padded blocks
-    'padded-blocks': [0]
-  }
+  extends: [
+    'airbnb/base',
+    'timkurvers/base'
+  ]
 };
