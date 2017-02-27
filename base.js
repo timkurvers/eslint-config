84cc5a35c997a19f071bede2db898bd491b82e70
Tim Kurvers <tim@moonsphere.net>
2017-02-27 21:07:44 +0100

    Add dangling commas

--- base.js
+++ base.js
@@ -4,5 +4,5 @@ module.exports = {
     // Enforce using all arguments
     // Allow ignoring arguments with underscore prefix
     'no-unused-vars': ['error', { args: 'all', argsIgnorePattern: '^_' }],
-  }
+  },
 };
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: [
     'airbnb/base',
-    'timkurvers/base'
-  ]
+    'timkurvers/base',
+  ],
 };
