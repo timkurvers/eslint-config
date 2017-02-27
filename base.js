d68a77ebd9986ed545d118192e878a94c6126ccf
Tim Kurvers <tim@moonsphere.net>
2017-02-27 21:04:09 +0100

    Do not allow padded blocks

--- base.js
+++ base.js
@@ -4,8 +4,5 @@ module.exports = {
     // Enforce using all arguments
     // Allow ignoring arguments with underscore prefix
     'no-unused-vars': [2, { args: 'all', argsIgnorePattern: '^_' }],
-
-    // Allow padded blocks
-    'padded-blocks': [0]
   }
 };
